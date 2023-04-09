0d64af6fe1919d24010bde457e52b019ad882ecb
Victor <victor.chumba@student.moringaschool.com>
2023-04-10 02:49:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// Write your code in this file
+let currentUser = "CHILD"
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
+let firstInitial = currentUser[0];
+let restOfName = currentUser.slice(1);
+let shortGreeting = "Welcome, " + firstInitial + "!"
+
+
+
+
+
